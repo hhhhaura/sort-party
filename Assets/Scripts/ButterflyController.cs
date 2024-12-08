2810ad2fe51e9e50f0493d840a7396fd5f194802
wama <36957874+april1026@users.noreply.github.com>
2024-12-08 17:03:14 +0800

    feat: add butterfly animation & controller

--- Assets/Scripts/ButterflyController.cs
+++ Assets/Scripts/ButterflyController.cs
@@ -6,24 +6,60 @@ public class ButterflyController : MonoBehaviour {
 
     public bool isPresent;
     public float timeToArrived;
+    public bool isBlown;
+    public float timeToBeBlown;
 
 
+    public Vector3 blowDirection = new Vector3(0, 1, 2);
+    public float blowForce = 1;
     public bool setStartPosition;
     public Vector3 startPosition;
     public bool setTargetPosition;
     public Vector3 targetPosition;
+    public bool reset;
 
-    private float time;
+    private float moveTime;
+    private float blownTime;
+    public Animator butterflyAnimator;
+    public Rigidbody butterflyRigidbody;
     public GameObject butterflyGameObject;
+    private bool isEntered;
 
     // Start is called before the first frame update
     void Start() {
         isPresent = false;
         setStartPosition = false;
         setTargetPosition = false;
-        timeToArrived = 0.5f;
+        isEntered = false;
+        isBlown = false;
+        reset = false;
+        timeToArrived = 1.04f;
+        moveTime = 0.0f;
+        timeToBeBlown = 1.0f;
+        blownTime = 0.0f;
 
         butterflyGameObject = gameObject.transform.Find("Butterfly").gameObject;
+        butterflyAnimator = butterflyGameObject.GetComponent<Animator>();
+        butterflyRigidbody = butterflyGameObject.GetComponent<Rigidbody>();
+        
+        butterflyGameObject.SetActive(false);
+    }
+
+    void FixedUpdate() {
+        if (isBlown) {
+            if (blownTime <= timeToBeBlown) {
+                butterflyAnimator.SetBool("blown", true);
+                butterflyRigidbody.AddForce(blowDirection * blowForce);
+                blownTime += Time.deltaTime;
+            }
+            if (blownTime > timeToBeBlown) {
+                butterflyGameObject.SetActive(false);
+            }
+        } else {
+            blownTime = 0.0f;
+            resetPool(butterflyRigidbody);
+            butterflyAnimator.SetBool("blown", false);
+        }
     }
 
     // Update is called once per frame
@@ -36,10 +72,25 @@ public class ButterflyController : MonoBehaviour {
             setTargetPositionWithNowPosition();
             setTargetPosition = false;
         }
+        if (reset) {
+            resetButterfly();
+            reset = false;
+        }
 
-        if (isPresent) {
-            time += Time.deltaTime/timeToArrived;
-            butterflyGameObject.transform.position = Vector3.Lerp(startPosition, targetPosition, time);
+        if (isPresent && isBlown != true) {
+            butterflyGameObject.SetActive(true);
+            if (isEntered) {
+                butterflyAnimator.SetBool("flyin", false);
+            }
+            if (isEntered != true) {
+                butterflyAnimator.SetBool("flyin", true);
+                isEntered = true;
+            }
+            moveTime += Time.deltaTime/timeToArrived;
+            butterflyGameObject.transform.position = Vector3.Lerp(startPosition, targetPosition, moveTime);
+        } else {
+            moveTime = 0.0f;
+            isEntered = false;
         }
     }
 
@@ -49,5 +100,19 @@ public class ButterflyController : MonoBehaviour {
 
     void setTargetPositionWithNowPosition() {
         targetPosition = butterflyGameObject.transform.position;
+    }
+
+    void resetButterfly() {
+        isPresent = false;
+        isEntered = false;
+        isBlown = false;
+        moveTime = 0.0f;
+        blownTime = 0.0f;
+        butterflyGameObject.SetActive(false);
+    }
+
+    public void resetPool(Rigidbody rg) {
+        rg.velocity = Vector3.zero;
+        rg.angularVelocity = Vector3.zero;
     }
 }
